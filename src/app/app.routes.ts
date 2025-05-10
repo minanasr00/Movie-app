af0bedbc35cdf250cd9960859c8daf2dde598f0f
mariem-mourad <mariemmouraad@gmail.com>
2025-05-10 05:21:13 +0300

    update movie search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { WishListComponent } from './components/wish-list/wish-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { SearchMovieComponent } from './components/search-movie/search-movie.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent ,title:"Movie App" },
   { path: "wish-list", component: WishListComponent },
   { path: "movie-details/:id", component: MovieDetailsComponent },
+  {path: "search/:query", component: SearchMovieComponent},
   {path:"**" , redirectTo : '' ,pathMatch:"full" }
 ];
