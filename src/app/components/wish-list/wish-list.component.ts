d087e822c2e67989e15858946f168eca1d6ad784
minanasr00 <62456457+minanasr00@users.noreply.github.com>
2025-05-10 10:37:32 +0300

    edit wish-list

--- src/app/components/wish-list/wish-list.component.ts
+++ src/app/components/wish-list/wish-list.component.ts
@@ -1,30 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { WishlistService } from '../../services/wishlist.service';
-
-// @Component({
-//   selector: 'app-wishlist',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './wish-list.component.html',
-//   styleUrls: ['./wish-list.component.css']
-// })
-// export class WishlistComponent implements OnInit {
-//   movies: any[] = [];
-
-//   constructor(private wishlistService: WishlistService) {}
-
-//   ngOnInit() {
-//   this.wishlistService.getMovies().subscribe(data => {
-//     console.log(data);
-//     this.movies = data;
-//   });
-// }
-
-//   removeFromWishlist(index: number) {
-//     this.movies.splice(index, 1);
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
 import { Movie1 } from '../../interfaces/movie';
